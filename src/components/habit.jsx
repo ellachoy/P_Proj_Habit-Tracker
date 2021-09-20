d6db0651e1fc0cf592be46aa2b409898c359b005
Ella Choy <egal21de@gmail.com>
2021-09-21 00:48:48 +0200

    handlefunc.

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -11,7 +11,10 @@ class Habit extends Component {
       this.setState({count: this.state.count +1 })
     };
     handleDecrement = () =>{
-         this.setState({count: this.state.count -1 })
+
+        //  this.setState({count: this.state.count -1 }) // minus zahlen abgezeigt deshalb unten code
+        const count = this.state.count -1;
+        this.setState({count:count <0 ? 0:count})
        };
     render() {
         return ( 
